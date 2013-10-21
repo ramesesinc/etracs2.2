d409bc4cb62ee30742a9f0bd2abc514253428270
Arnel Retiza <retiza.arnel@gmail.com>
2013-10-21 10:37:11 +0800

    escape maxvalue

--- services/rpt-services.jar/META-INF/sql/rpu_mssql.sql
+++ services/rpt-services.jar/META-INF/sql/rpu_mssql.sql
@@ -1,7 +1,7 @@
 [getRPTParameter]
 SELECT 
     objid AS paramid, paramname, paramcaption, paramdesc AS description, 
-    paramtype AS datatype,  parammin AS minvalue, parammax AS `maxvalue`  
+    paramtype AS datatype,  parammin AS minvalue, parammax AS maxvalue 
 FROM rptparameters WHERE paramname = $P{name} 
 
 [getParamaterList]
